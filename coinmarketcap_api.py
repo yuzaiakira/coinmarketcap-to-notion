32874e455a3ed310154d34117a84563d9cb9f47a
Akira <yuzai.akira@gmail.com>
2024-05-18 20:21:13 +0000

    fix some bug

--- coinmarketcap_api.py
+++ coinmarketcap_api.py
@@ -1,21 +1,21 @@
-
 from requests import Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 
 
 class CoinMarketCap:
-  def __init__(self, key: str, url:str, parameters: dict) -> None:
+
+  def __init__(self, key: str, url: str, parameters: dict) -> None:
     self.API_KEY = key
     self.URL = url
     self.PARAMETERS = parameters
     self.DATA = self.call()
-    
-  def call(self) -> dict:    
+
+  def call(self) -> dict:
     session = Session()
     headers = {
-      'Accepts': 'application/json',
-      'X-CMC_PRO_API_KEY': self.API_KEY,
+        'Accepts': 'application/json',
+        'X-CMC_PRO_API_KEY': self.API_KEY,
     }
     session.headers.update(headers)
     try:
@@ -24,14 +24,11 @@ class CoinMarketCap:
       return data
     except (ConnectionError, Timeout, TooManyRedirects) as e:
       raise e
-    
+
   def clean_coins(self):
     data = self.DATA['data']
     coins = dict()
     for coin in data.keys():
       coins[coin] = data[coin]
-      
+
     return coins
-  
-    
-    
--- notion/client.py
+++ notion/client.py
@@ -1,10 +1,8 @@
 from .base import BaseClient
 from .database import Database
 from .page import Page
-from pprint import pprint
 
 
-    
 class Client(BaseClient, Database, Page):
     properties = {
         'symbol': 'title',
@@ -20,10 +18,11 @@ class Client(BaseClient, Database, Page):
         'name': 'rich_text',
         'description': 'rich_text',
         'tags': 'multi_select',
-        'platform': 'select',
-        'category': 'select',
+        'platform': 'rich_text',
+        'category': 'rich_text',
     }
-    def get_symbols(self)-> list[str] | None:
+
+    def get_symbols(self) -> list[str] | None:
         """get all used symbols in Notion database
 
         Returns:
@@ -35,13 +34,13 @@ class Client(BaseClient, Database, Page):
         if data:
             for row in data['results']:
                 symbol = row['properties']['symbol']['title']
-                if symbol :
+                if symbol:
                     symbols.append(symbol[0]['text']['content'])
-                    
+
             return symbols
-        
+
         return None
-    
+
     def clean_pages_data(self, data: dict):
 
         clean_data = dict()
@@ -51,61 +50,102 @@ class Client(BaseClient, Database, Page):
                 coin = coin[0]
 
             clean_data[symbol] = {
-                'website': {'rich_text': [{"text": {
-                                                "content": '\n'.join(coin['urls']['website'])
-                                            }}]},
-                
-                'technical_doc':  {'rich_text': [{"text": {
-                                                "content": '\n'.join(coin['urls']['technical_doc'])
-                                            }}]},
-                
-                'twitter': {'rich_text': [{"text": {
-                                                "content": '\n'.join(coin['urls']['twitter'])
-                                            }}]},
-                
-                'reddit':  {'rich_text': [{"text": {
-                                                "content": '\n'.join(coin['urls']['reddit'])
-                                            }}]},
-                
-                'message_board':  {'rich_text': [{"text": {
-                                                "content": '\n'.join(coin['urls']['message_board'])
-                                            }}]},
-                
-                'announcement':  {'rich_text': [{"text": {
-                                                "content": '\n'.join(coin['urls']['announcement'])
-                                            }}]},
-                
-                'chat':  {'rich_text': [{"text": {
-                                                "content": '\n'.join(coin['urls']['chat'])
-                                            }}]},
-                
-                'explorer':  {'rich_text': [{"text": {
-                                                "content": '\n'.join(coin['urls']['explorer'])
-                                            }}]},
-                
-                'source_code': {'rich_text': [{"text": {
-                                                "content": '\n'.join(coin['urls']['source_code'])
-                                            }}]},
-                
-                'name': {'rich_text': [{"text": {
-                                                "content": coin['name']
-                                            }}]},
-                
-                'description': {'rich_text': [{"text": {
-                                                "content": coin['description']
-                                            }}]},
-                
-                'tags': {'multi_select': [{"name": i } for i in coin['tags']]},
-                
-                'platform': {'select': {"name": str(coin['platform']) }},
-                'category': {'select': {"name": coin['category'] }},
+                'website': {
+                    'rich_text': [{
+                        "text": {
+                            "content": '\n'.join(coin['urls']['website'])
+                        }
+                    }]
+                },
+                'technical_doc': {
+                    'rich_text': [{
+                        "text": {
+                            "content": '\n'.join(coin['urls']['technical_doc'])
+                        }
+                    }]
+                },
+                'twitter': {
+                    'rich_text': [{
+                        "text": {
+                            "content": '\n'.join(coin['urls']['twitter'])
+                        }
+                    }]
+                },
+                'reddit': {
+                    'rich_text': [{
+                        "text": {
+                            "content": '\n'.join(coin['urls']['reddit'])
+                        }
+                    }]
+                },
+                'message_board': {
+                    'rich_text': [{
+                        "text": {
+                            "content": '\n'.join(coin['urls']['message_board'])
+                        }
+                    }]
+                },
+                'announcement': {
+                    'rich_text': [{
+                        "text": {
+                            "content": '\n'.join(coin['urls']['announcement'])
+                        }
+                    }]
+                },
+                'chat': {
+                    'rich_text': [{
+                        "text": {
+                            "content": '\n'.join(coin['urls']['chat'])
+                        }
+                    }]
+                },
+                'explorer': {
+                    'rich_text': [{
+                        "text": {
+                            "content": '\n'.join(coin['urls']['explorer'])
+                        }
+                    }]
+                },
+                'source_code': {
+                    'rich_text': [{
+                        "text": {
+                            "content": '\n'.join(coin['urls']['source_code'])
+                        }
+                    }]
+                },
+                'name': {
+                    'rich_text': [{
+                        "text": {
+                            "content": coin['name']
+                        }
+                    }]
+                },
+                'description': {
+                    'rich_text': [{
+                        "text": {
+                            "content": coin['description']
+                        }
+                    }]
+                },
+                'tags': {
+                    'multi_select': [{
+                        "name": i
+                    } for i in coin['tags']]
+                },
+                'platform': {
+                    'rich_text': [{
+                        "text": {
+                            "content": str(coin['platform'])
+                        }
+                    }]
+                },
+                'category': {
+                    'rich_text': [{
+                        "text": {
+                            "content": coin['category']
+                        }
+                    }]
+                },
             }
 
-            
-            
         return clean_data
-
-    
-
-        
-       
